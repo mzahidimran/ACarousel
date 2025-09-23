cc6b6fbc3f93b77ccc04c928635b720dac6066f4
Muhammad Zahid Imran <m_zahidimran@yahoo.com>
2025-09-23 19:52:55 +0500

    Update ACarouselViewModel.swift
    fix RTL

--- Sources/ACarousel/ACarouselViewModel.swift
+++ Sources/ACarousel/ACarouselViewModel.swift
@@ -246,7 +246,7 @@ extension ACarouselViewModel {
         /// Avoid dragging more than the values of multiple subviews at the end of the drag,
         /// and still only one subview is toggled
         ///Support RTL
-        if UIApplication.shared.userInterfaceLayoutDirection == .rightToLeft {
+        if UIView.appearance().semanticContentAttribute == .forceLeftToRight {
 
         var offset: CGFloat = itemActualWidth
         if value.translation.width > 0 {
